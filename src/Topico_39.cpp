6bbde5667ca02a2a4d20bb843612922af8998811
Matheus Guimarães <matheusguimr@gmail.com>
2018-09-03 22:22:30 -0300

    Add the source code from topic 37 to 40

---
+++ src/Topico_39.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+    Mat img, grayImg, otsuThresholdImg, structuringElement;
+
+    img = imread("/home/matheus/Dropbox/treinamento-pdi/samples/black_lightning.jpg");
+
+    cvtColor(img, grayImg, CV_RGB2GRAY);
+
+    GaussianBlur(grayImg, grayImg, Size(3, 3), 1);
+
+    threshold(grayImg, otsuThresholdImg, 0, 255, THRESH_OTSU);
+
+    structuringElement = getStructuringElement(MORPH_RECT, Size(1, 3), Point(-1, -1));
+
+    imshow("Otsu's threshold image", otsuThresholdImg);
+
+    for (int i = 0; i < 5; ++i) {
+        erode(otsuThresholdImg, otsuThresholdImg, structuringElement, Point(-1, -1), i);
+        imshow("eroded image with vertical structuring element", otsuThresholdImg);
+    }
+
+    imwrite("/home/matheus/Dropbox/treinamento-pdi/results/39_black_lightning_eroded_vertical_element.jpg", otsuThresholdImg);
+
+    waitKey(0);
+    return 0;
+}
+
